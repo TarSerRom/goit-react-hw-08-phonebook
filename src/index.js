ebfa7f564aa663a2d670c6af6be13e4efd445327
TarSerRom <tarasovsergeyrom@gmail.com>
2022-04-28 17:21:47 -0500

    css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from 'redux/store';
+
+import 'react-toastify/dist/ReactToastify.css';
 import  App from 'components/App';
 import './index.css';
 
